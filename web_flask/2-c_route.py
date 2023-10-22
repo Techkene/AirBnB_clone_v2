dad1aaa8b9a7287bc00ed2e5099e1ceb2c15a534
Techkene <keneanoliefo24@gmail.com>
2023-10-23 00:50:39 +0100

    fourth task'

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -22,15 +22,8 @@ def hbnb():
 
 @app.route("/c/<text>", strict_slashes=False)
 def cText(text):
-        """display C followed by the value of the text variable"""
-                return "C {}".format(text.replace("_", " "))
-
-
-@app.route('/python', strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def pythonText(text="is cool"):
-    """display Python followed by the value of the text variable"""
-    return "Python {}".format(text.replace("_", " "))
+    """display C followed by the value of the text variable"""
+    return "C {}".format(text.replace("_", " "))
 
 
 if __name__ == "__main__":
